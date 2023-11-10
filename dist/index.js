12ee0c69f32041297dee2932e297f396d8b31832
safiyama <k-akiyama@safie.jp>
2023-11-10 18:25:59 +0900

    更新

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,24 @@
 "use strict";
-const hello = 'Hello 秋山!';
-console.log(hello);
+Object.defineProperty(exports, "__esModule", { value: true });
+const rxjs_1 = require("rxjs");
+(0, rxjs_1.of)(5)
+    .pipe((0, rxjs_1.mergeMap)((value) => {
+    return (0, rxjs_1.range)(0, value).pipe((0, rxjs_1.zipWith)((0, rxjs_1.interval)(1000)));
+}), (0, rxjs_1.tap)((value) => console.log(value)), (0, rxjs_1.map)((values) => values[0]))
+    .subscribe((value) => console.log(value));
+const subject = new rxjs_1.Subject();
+subject.pipe((0, rxjs_1.concatMap)((obj) => {
+    return httpGet(obj.url, obj.delay);
+}));
+subject.next({ url: 'foo', delay: 500 });
+subject.next({ url: 'bar', delay: 300 });
+subject.next({ url: 'baz', delay: 100 });
+function httpGet(url, delay) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const obj = { ResultData: url + ' -> resolved' };
+            resolve(JSON.stringify(obj));
+        }, delay);
+    });
+}
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,34 @@
-const hello: string = 'Hello 秋山!';
-console.log(hello);
+import { Subject, concat, concatMap, interval, map, mergeMap, of, range, tap, zipWith } from 'rxjs';
+
+of(5)
+  .pipe(
+    mergeMap((value) => {
+      return range(0, value).pipe(zipWith(interval(1000)));
+    }),
+    tap((value) => console.log(value)),
+    map((values) => values[0])
+  )
+  .subscribe((value) => console.log(value));
+
+const subject = new Subject<{ url: string; delay: number }>();
+
+subject
+  .pipe(
+    concatMap((obj) => {
+      return httpGet(obj.url, obj.delay);
+    })
+  )
+  .subscribe((value) => console.log(value));
+
+subject.next({ url: 'foo', delay: 500 });
+subject.next({ url: 'bar', delay: 300 });
+subject.next({ url: 'baz', delay: 100 });
+
+function httpGet(url: string, delay: number) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const obj = { ResultData: url + ' -> resolved' };
+      resolve(JSON.stringify(obj));
+    }, delay);
+  });
+}
